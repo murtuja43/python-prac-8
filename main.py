1de1ea14978b1ad9ab55c001f4b202f90c736810
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2024-05-15 21:53:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ def divide(x, y):
         return "Error! Division by zero."
     else:
         return x / y
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
